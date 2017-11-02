bea6c97fdf92cef8a0ec0c2e63af87e5d575d0ec
Minotus <panu85@suomi24.fi>
2017-11-02 21:19:56 +0200

    Init commit

---
+++ NetatmoCore.Tests/AuthorizationTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using netatmocore;
+
+namespace NetatmoCore.Tests
+{
+    public class AuthorizationTests
+    {
+        [Fact]
+        public void Authorization_Success_With_Correct_User()
+        {
+            // Fill these with own values for testing purpose
+            var clientId = "";
+            var clientSecret = "";
+            var username = "";
+            var password = "";
+
+            var auth = new NetatmoAuth();
+            var token = auth.Login(clientId, clientSecret, username, password, new[] { NetatmoAuth.READ_STATION});
+
+            var netatmo = new NetAtmoClient(token.access_token);
+            
+            var result = netatmo.Getthermostatsdata("CloudAtmo").Result;
+        }
+    }
+}
---
+++ NetatmoCore/NetAtmoClient.cs
@@ -0,0 +1,56 @@
+﻿using netatmocore.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NetatmoCore.Helpers;
+
+namespace NetatmoCore
+{
+    public class NetAtmoClient
+    {
+        /// <summary>
+        /// Retrieved with NetatmoAuth class
+        /// </summary>
+        public string AccessToken { get; set; }
+
+        private HttpClient httpClient;
+
+        private const string BASE_URI = "https://api.netatmo.com/api/";
+
+        public NetAtmoClient(string access_token)
+        {
+            AccessToken = access_token;
+            Initialize();
+        }
+
+        protected void Initialize()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(BASE_URI);
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded");
+        }
+
+        /// <summary>
+        /// Returns data from a user Weather Stations (measures and device specific data).
+        /// <see cref="https://dev.netatmo.com/resources/technical/reference/weatherstation/getstationsdata"/>
+        /// </summary>
+        /// <returns></returns>
+        public async Task<NAStationDataResponse> Getthermostatsdata(string deviceId)
+        {
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("device_id", deviceId);
+            var thermostatDataJson = await httpClient.PostAsync(UriHelper.ResourceUriBuilder(httpClient, "getstationsdata", AccessToken), null);
+
+            if (!thermostatDataJson.IsSuccessStatusCode)
+                throw new Exception("Server returned " + thermostatDataJson.StatusCode.ToString() + " - Check access_token and device_id parameter.");
+
+            var thermostatData = JsonConvert.DeserializeObject<NAStationDataResponse>(await thermostatDataJson.Content.ReadAsStringAsync());
+
+            if (thermostatData == null || thermostatData.Body == null)
+                throw new Exception("Unable to deserialize reply into NAStationDataResponse. Possible version mismatch.");
+
+            return thermostatData;
+        }
+
+    }
+}
