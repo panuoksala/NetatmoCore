7cfc1002748d216c8c60fe65a87142bb19d639fd
Minotus <panu85@suomi24.fi>
2017-11-07 21:15:27 +0200

    Implemented Getpublicdata

--- NetatmoCore/NetAtmoClient.cs
+++ NetatmoCore/NetAtmoClient.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using NetatmoCore.Helpers;
+using System.Globalization;
 
 namespace NetatmoCore
 {
@@ -50,6 +51,40 @@ namespace NetatmoCore
                 throw new Exception("Unable to deserialize reply into NAStationDataResponse. Possible version mismatch.");
 
             return thermostatData;
+        }
+
+        /// <summary>
+        /// Returns public data from matching stations in given area.
+        /// <see cref="https://dev.netatmo.com/en-US/resources/technical/reference/weatherapi/getpublicdata"/>
+        /// </summary>
+        /// <param name="latitude_ne"></param>
+        /// <param name="longitude_ne"></param>
+        /// <param name="latitude_sw"></param>
+        /// <param name="longitude_sw"></param>
+        /// <param name="required_data"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public async Task<NAPublicDataResponse> Getpublicdata(int latitude_ne, int longitude_ne, int latitude_sw, int longitude_sw, string[] required_data, bool filter)
+        {
+            var uri = UriHelper.ResourceUriBuilder(httpClient, "getpublicdata", AccessToken);
+            uri.AddQuery("lat_ne", latitude_ne.ToString(CultureInfo.InvariantCulture))
+                .AddQuery("lon_ne", longitude_ne.ToString(CultureInfo.InvariantCulture))
+                .AddQuery("lat_sw", latitude_sw.ToString(CultureInfo.InvariantCulture))
+                .AddQuery("lon_dw", longitude_sw.ToString(CultureInfo.InvariantCulture))
+                .AddQuery("required_data", string.Join(",",required_data))
+                .AddQuery("filter", filter.ToString(CultureInfo.InvariantCulture));
+
+            var publicDataJson = await httpClient.GetAsync(uri);
+
+            if (!publicDataJson.IsSuccessStatusCode)
+                throw new Exception("Server returned " + publicDataJson.StatusCode.ToString() + " - Check access_token and device_id parameter.");
+
+            var publicData = JsonConvert.DeserializeObject<NAPublicDataResponse>(await publicDataJson.Content.ReadAsStringAsync());
+
+            if (publicData == null || publicData.Body == null)
+                throw new Exception("Unable to deserialize reply into NAStationDataResponse. Possible version mismatch.");
+
+            return publicData;
         }
 
     }
